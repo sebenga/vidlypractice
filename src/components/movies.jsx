0c872608837ec104acae116d154e3eadc12f41f4
Tankiso Sebenga <sebengat@emea.objectiflune.com>
2018-10-05 15:21:54 +0200

    3 commit delete

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,11 +5,19 @@ class Movies extends Component {
         movies: getMovies()
      }
      handleDelete = (movie)=>{
-        console.log(movie);
+        const movies = this.state.movies.filter(m=>m._id !== movie._id);
+        this.setState({movies});
      }
 
     render() { 
-        return <table className="table">
+        const{ length: count} = this.state.movies
+        if(count=== 0){
+            return<p>There are no movies in the database</p>;
+        }
+        return( 
+        <React.Fragment>
+        <p>Showing{count} movies in the database</p>
+        <table className="table">
             <thead>
                 <tr>
                     <th>Title</th>
@@ -26,11 +34,16 @@ class Movies extends Component {
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
-                    <td><button onclick={()=>this.handleDelete(movie)} className="btn btn-danger btn-sam">delete</button></td>
+                    <td><button 
+                        onClick={()=>this.handleDelete(movie)} 
+                        className="btn btn-danger btn-sam">
+                        delete
+                        </button>
+                    </td>
                 </tr>))}
-                
             </tbody>
         </table>
+    </React.Fragment>)
     }
 }
  
